821f3dd72644d7d3ace0bad55b9ecda48b6fb900
regorDam <regorm88@gmail.com>
2016-06-21 05:34:52 +0200

    5.34 am

---
+++ Assets/Scripts/TileBackground.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileBackground : MonoBehaviour 
+{
+    Color color = Color.blue;
+    float fColor = 0f;
+    float fNumber = 0f;
+    bool op = true;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        fColor += 0.01f * Time.deltaTime ;
+
+        Vector2 calc = Vector2.zero;
+
+        if (fNumber <= 0.3)
+            op = true;
+        if (fNumber >= 0.85)
+            op = false;
+        if(op)
+            fNumber += 0.15f * Time.deltaTime;
+        else
+            fNumber -= 0.35f * Time.deltaTime;
+ 
+        
+        color = Color.HSVToRGB(fColor, 1, fNumber, true);
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Standard");
+        rend.material.SetColor("_EmissionColor", color);
+
+        if(fColor > 256)
+        {
+            fColor = 0;
+        }
+        
+        //Debug.Log(color);
+	
+	}
+}
